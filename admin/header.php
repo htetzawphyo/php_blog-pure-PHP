423aabb5e8d91a11634b07d93a7b885dd973c836
Htet Zaw Phyo <htetzawphyo.hpn@gmail.com>
2022-07-30 15:43:10 +0630

    token bug

--- admin/header.php
+++ admin/header.php
@@ -45,6 +45,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
            ?>
 
           <form class="form-inline" method="post" action="<?php echo $page == 'index.php'? 'index.php' : 'user_list.php'; ?>">
+            <input type="hidden" name="_token" value="<?php echo $_SESSION['_token'] ?>">
             <div class="input-group input-group-sm">
               <input name="search" class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
               <div class="input-group-append">
--- config/common.php
+++ config/common.php
@@ -1,5 +1,14 @@
 <?php
 
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(!hash_equals($_SESSION['_token'], $_POST['_token'])){
+        echo 'Invalid CSRF token';
+        die();
+    }else {
+        unset($_SESSION['_token']);
+    }
+}
+
 if(empty($_SESSION['_token'])){
     if(function_exists('random_bytes')){
         $_SESSION['_token'] = bin2hex(random_bytes(32));
@@ -7,15 +16,6 @@ if(empty($_SESSION['_token'])){
         $_SESSION['_token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
     } else {
         $_SESSION['_token'] = bin2hex(openssl_random_pseudo_bytes(32));
-    }
-}
-
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    if(!hash_equals($_SESSION['_token'], $_POST['_token'])){
-        echo 'Invalid CSRF token';
-        die();
-    }else {
-        unset($_SESSION['_token']);
     }
 }
 
